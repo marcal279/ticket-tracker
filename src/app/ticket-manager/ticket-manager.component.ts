394ac76b07ed188cce1267331862f24711f21257
Marc Almeida <marcal2791@gmail.com>
2022-03-21 15:51:37 +0530

    Updated stylesheets and ticket-manager

--- src/app/ticket-manager/ticket-manager.component.ts
+++ src/app/ticket-manager/ticket-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ticket } from '../ticket';
 
 @Component({
   selector: 'app-ticket-manager',
@@ -12,6 +13,26 @@ export class TicketManagerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.generateSampleTickets();
   }
+
+  sampleTickets: Ticket[] = [];
+  generateSampleTickets(){
+    for(let i=1; i<=5;i++){
+      let newTicket: Ticket = {
+        tid: String(50 + i),
+        // empid: Math.random()>0.5?'NXT1234':'NXT9876',
+        empid: 'NXT1234',
+        title: 'Ticket No. ' + String(50 + i),
+        desc: Math.random() > 0.5 ? 'desc' : null,
+        status: ['Pending', 'Production', 'Testing', 'Approval', 'Closed'][Math.floor(Math.random() * 5)],
+        duration: String(Math.ceil(Math.random()*3))+'w',
+      }
+      this.sampleTickets.push(newTicket);
+    }
+  }
+  displayedColumns: string[] = ['tid','title','desc','duration','status'];
+  dataSource = this.sampleTickets;
+
 
 }
