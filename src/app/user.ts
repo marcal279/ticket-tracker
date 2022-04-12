abcc663c21e8ae64bc5902f0892df7bfea76157f
Marc Almeida <marcal2791@gmail.com>
2022-04-12 18:19:31 +0530

    Refactored User structure

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,8 +1,13 @@
 export interface User{
+    empEid: string,      // use email as username, later maybe empID too
     name: string,
-    empID: string,      // can use empID or email as username
-    email: string,
     password: string,   // later could hash this value
-    picture: null,      // later check how to add this (profile picture)
+    // picture: null,      // later check how to add this (profile picture)
+    role: string,
+    mob: string,
+    company: string,    // NDL or IMCL
     dept: string,       // Finance, Ops, Legal, Logistics
+    designation: string, 
+    supervisor: string,
+    offExt: string
 }
