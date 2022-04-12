a46a561457b3daa19fe5d53fca510ca66db3dce6
Marc Almeida <marcal2791@gmail.com>
2022-04-12 18:20:02 +0530

    User Create & Read added

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,11 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+  currPage = 'My Profile';
+  USERS: any[] = [];
 
-  currPage = 'My Profile'
-  constructor() { }
+  constructor( public userService: UserService ) { }
 
-  ngOnInit(): void {
+  createNewUser():User{
+    return this.userService.newUserObject();
   }
 
+  newUser!:User // = this.createNewUser();
+  currUser!: User // = this.newUser;
+
+  metadata(){
+    alert(JSON.stringify(this.currUser))
+  }
+
+  addUser(){
+    this.userService.createDBUser(this.currUser);
+  }
+
+  retrieveUsers(){
+    this.userService.readDBUsers().subscribe(observer => {
+      this.USERS = observer.map((e)=>{
+        return{
+          key: e.payload.doc.id,
+          ...(e.payload.doc.data() as User)
+        }
+      })
+    })
+  }
+
+  ngOnInit(): void {
+    this.newUser = this.createNewUser();
+    this.currUser = this.newUser;
+    this.retrieveUsers();
+  }
 }
