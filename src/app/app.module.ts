b105cabab5b6eedcaeec9687fe06250bd0d9d642
Marc Almeida <marcal2791@gmail.com>
2022-04-13 18:13:38 +0530

    Created Sign Up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database'; // for tickets
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
     SideNavComponent,
     TicketManagerComponent,
     TicketDialogComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
