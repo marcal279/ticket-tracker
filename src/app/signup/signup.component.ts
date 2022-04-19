1147b718079356e8093f2e5cd803bd478bf49788
Marc Almeida <marcal2791@gmail.com>
2022-04-19 18:40:53 +0530

    Sign Up logic added, TODO valid tick

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  openSnackBar(message: string, action: string = 'Close') {
+    this.matSnack.open(message, action);
+  }
+
+  constructor(public userService: UserService, public matSnack: MatSnackBar) { }
+
+  newUser !: User;
+
+  passwordVal = '';
+  passwordMatchVal = '';
+
+  checkPasswordValid(){
+    if(this.passwordVal.length>=4 && !this.passwordVal.includes(' ') && this.passwordVal.match(/^[a-zA-Z]\w$/)) return true;
+    return false;
+  }
+
+  checkPasswordMatch(){
+    // add password valid ticks pls
+    if(this.passwordVal === this.passwordMatchVal) return true;
+    return false;
+  }
+
+  signUp(){
+    if(this.checkPasswordMatch()){ 
+      this.newUser.password = this.passwordVal;
+      this.userService.createDBUser(this.newUser); 
+      this.openSnackBar(`Created user ${this.newUser.name}`)
+    }
+    else this.openSnackBar("Passwords don't match, please try again")
+  }
 
   ngOnInit(): void {
+    this.newUser = this.userService.newUserObject();
   }
 
 }
