14e26530a4187cad5473b2bd99d1d113c526e124
Marc Almeida <marcal2791@gmail.com>
2022-04-21 15:13:07 +0530

    Added Firestore UID field for user
    Corresponds to key in Firestore

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,13 +1,14 @@
 export interface User{
+    uid ?: string,
     empEid: string,      // use email as username, later maybe empID too
     name: string,
     password: string,   // later could hash this value
     // picture: null,      // later check how to add this (profile picture)
-    role: string,
+    role?: string,
     mob: string,
     company: string,    // NDL or IMCL
     dept: string,       // Finance, Ops, Legal, Logistics
     designation: string, 
-    supervisor: string,
-    offExt: string
+    supervisor?: string,
+    offExt?: string
 }
