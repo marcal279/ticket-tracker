0dfdc9ac36a5f82b527259031d4271a60af2bf2b
Marc Almeida <marcal2791@gmail.com>
2022-05-19 15:50:42 +0530

    Updated to Ticket Tracker DB's, not Ticket Master

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBiEM_LzSPhoeoicw1v2mmqoc4BAUSEqnM",
-    authDomain: "nxt-ticket-master.firebaseapp.com",
-    databaseURL: "https://nxt-ticket-master-default-rtdb.firebaseio.com",
-    projectId: "nxt-ticket-master",
-    storageBucket: "nxt-ticket-master.appspot.com",
-    messagingSenderId: "247898630124",
-    appId: "1:247898630124:web:ecc199c3c4eb2d07cec2c9",
-    measurementId: "G-VVZ4FJXDL9"
+    apiKey: "AIzaSyBx7U0qJwj6HEX4eesZ6TdiRK8dVrfEk-4",
+    authDomain: "ticket-tracker-nxt-imcl.firebaseapp.com",
+    databaseURL: "https://ticket-tracker-nxt-imcl-default-rtdb.firebaseio.com",
+    projectId: "ticket-tracker-nxt-imcl",
+    storageBucket: "ticket-tracker-nxt-imcl.appspot.com",
+    messagingSenderId: "165514593894",
+    appId: "1:165514593894:web:9ea9de74400988c40f4fa7",
+    measurementId: "G-R5ZKL6DQHQ"
   }
 };
 
