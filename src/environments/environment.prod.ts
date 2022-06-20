94603cf789cbfd66bc2069f673555d17608db5d7
Marc Almeida <marcal2791@gmail.com>
2022-06-21 02:35:33 +0530

    Firebase environment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBx7U0qJwj6HEX4eesZ6TdiRK8dVrfEk-4",
-    authDomain: "ticket-tracker-nxt-imcl.firebaseapp.com",
-    databaseURL: "https://ticket-tracker-nxt-imcl-default-rtdb.firebaseio.com",
-    projectId: "ticket-tracker-nxt-imcl",
-    storageBucket: "ticket-tracker-nxt-imcl.appspot.com",
-    messagingSenderId: "165514593894",
-    appId: "1:165514593894:web:9ea9de74400988c40f4fa7",
-    measurementId: "G-R5ZKL6DQHQ"
+    apiKey: "AIzaSyAp4GLtNCnveoOycVpbBzysfQHqq7JZsgQ",
+    authDomain: "ticket-tracker-angular.firebaseapp.com",
+    databaseURL: "https://ticket-tracker-angular-default-rtdb.firebaseio.com",
+    projectId: "ticket-tracker-angular",
+    storageBucket: "ticket-tracker-angular.appspot.com",
+    messagingSenderId: "12579756642",
+    appId: "1:12579756642:web:fb40f8fabc71d8c8bd4f3b",
+    measurementId: "G-ECKG6NFRBG"
   }
 };
