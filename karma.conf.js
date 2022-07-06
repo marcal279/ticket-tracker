c75c697bff12af57451407b6b27b84c3b02324f3
Marc Almeida <marcal2791@gmail.com>
2022-07-06 13:19:24 +0530

    Updated files, project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nxt-ticket-master'),
+      dir: require('path').join(__dirname, './coverage/ticket-tracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
